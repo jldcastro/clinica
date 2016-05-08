38d99ad4ec6885d618b6e2e246b994939ba4f20b
jose luis duran castro <jlduran@alumnos.ubiobio.cl>
2016-05-08 06:11:40 -0400

    laravel 5.0 e instalación paquete Html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,27 @@
-@extends('app')
+@extends('layouts.base')
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+@section('title','Iniciar sesión usuario')
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+@section('contenido')
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+<section class="container">
+    <div class="login">
+        <h1>Iniciar Sesión</h1>
+            <form method="post" action="index.html">
+                <p><input type="text" name="login" value="" placeholder="Ingrese Email"></p>
+                <p><input type="password" name="password" value="" placeholder="Ingrese Contraseña"></p>
+              <p class="remember_me">
+                <label>
+                <input type="checkbox" name="remember_me" id="remember_me">
+                Recuerdame en este computador
+                </label>
+              </p>
+            <p class="submit"><input type="submit" name="commit" value="Iniciar sesión"></p>
+            </form>
+    </div>
+    <div class="login-help">
+        <a href="index.html">Olvidaste tu contraseña?</a>
+    </div>
+</section>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 @endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    <!-- Stylesheets -->
+        {!! Html::style('assets/css/bootstrap.min.css') !!}
+        {!! Html::style('assets/css/font-awesome.min.css') !!}
+        {!! Html::style('assets/css/style.css') !!}
+
+        <!-- Scripts -->
+        {!! Html::script('assets/js/jquery-1.11.3.min.js') !!}
+        {!! Html::script('assets/js/bootstrap.min.js') !!}
+
+</head>
+<body>
+
+@yield('contenido')
+
+</body>
+</html>
