3489ef77b80cd8804e013b9cc4b820d53a8becc3
jose luis duran castro <jlduran@alumnos.ubiobio.cl>
2016-05-08 07:52:17 -0400

    inicio de sesión enlazado con laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,21 +7,35 @@
 <section class="container">
     <div class="login">
         <h1>Iniciar Sesión</h1>
-            <form method="post" action="index.html">
-                <p><input type="text" name="login" value="" placeholder="Ingrese Email"></p>
-                <p><input type="password" name="password" value="" placeholder="Ingrese Contraseña"></p>
-              <p class="remember_me">
-                <label>
-                <input type="checkbox" name="remember_me" id="remember_me">
-                Recuerdame en este computador
-                </label>
-              </p>
-            <p class="submit"><input type="submit" name="commit" value="Iniciar sesión"></p>
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                    <p><input type="text" class = "form-control" name="login" value="{{ old('email') }}" placeholder="Ingrese Email"></p>
+                </div>
+                <div class="form-group">
+                    <p><input type="password" class="form-control" name="password" value="" placeholder="Ingrese Contraseña"></p>
+                </div>
+
+              <div class="form-group">
+                <p class="remember_me">
+                    <label>
+                        <input type="checkbox" name="remember_me" id="remember_me">
+                        Recuerdame en este computador
+                    </label>
+                </p>
+              </div>
+
+              <div class="form-group">
+                <p class="submit"><input type="submit" name="commit" value="Iniciar sesión"></p>
+              </div>
             </form>
-    </div>
-    <div class="login-help">
-        <a href="index.html">Olvidaste tu contraseña?</a>
-    </div>
+         </div>
+            <div class="login-help">
+                <a href="{{ url('/password/email') }}">Olvidaste tu contraseña?</a>
+                <a href="{{ url('/auth/register') }}">Registrar nuevo usuario</a>
+            </div>
+        </div>
 </section>
 
 @endsection
