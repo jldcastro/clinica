60a245899a4f2f61b30b444ac82ede0d69381462
jose luis duran castro <jlduran@alumnos.ubiobio.cl>
2016-05-08 11:14:35 -0400

    se modifica vista registrar y se agrega icono logo al navegador

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,14 +4,27 @@
 
 @section('contenido')
 
+
 <section class="container">
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+    	    <strong>Whoops!</strong> Hubo algunos problemas con tu entrada.<br><br>
+    		    <ul>
+    			    @foreach ($errors->all() as $error)
+    				<li>{{ $error }}</li>
+    				@endforeach
+    			</ul>
+    	</div>
+    @endif
+
     <div class="login">
         <h1>Iniciar Sesión</h1>
             <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                 <div class="form-group">
-                    <p><input type="text" class = "form-control" name="login" value="{{ old('email') }}" placeholder="Ingrese Email"></p>
+                    <p><input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Ingrese Email"></p>
                 </div>
                 <div class="form-group">
                     <p><input type="password" class="form-control" name="password" value="" placeholder="Ingrese Contraseña"></p>
@@ -20,20 +33,21 @@
               <div class="form-group">
                 <p class="remember_me">
                     <label>
-                        <input type="checkbox" name="remember_me" id="remember_me">
+                        <input type="checkbox" name="remember" id="remember_me">
                         Recuerdame en este computador
                     </label>
                 </p>
               </div>
 
               <div class="form-group">
-                <p class="submit"><input type="submit" name="commit" value="Iniciar sesión"></p>
+                <p class="submit"><input type="submit" value="Iniciar sesión"></p>
               </div>
+
             </form>
          </div>
             <div class="login-help">
                 <a href="{{ url('/password/email') }}">Olvidaste tu contraseña?</a>
-                <a href="{{ url('/auth/register') }}">Registrar nuevo usuario</a>
+                <a href="{{ url('/auth/register') }}">Registrarse</a>
             </div>
         </div>
 </section>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,6 +12,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo_vet.png') }}">
+
+
     <!-- Stylesheets -->
         {!! Html::style('assets/css/bootstrap.min.css') !!}
         {!! Html::style('assets/css/font-awesome.min.css') !!}
